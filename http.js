fec0952367d7aea67bcada01c977ee4f2bf88ea8
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-01-02 21:46:06 -0300

    HTTP configurando porta e host

--- http.js
+++ http.js
@@ -1,12 +1,26 @@
 // importanto o módulo http, doc: https://nodejs.org/api/http.html#class-httpserver
 const http = require('http');
 
+const PORT = 5000;
+const HOST = '127.0.0.1'
+
 // criar o servidor
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
+    if(req.method === 'GET') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Apenas permite GET')
+    } else {
+        res.statusCode = 405; // Não permitido
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Método não permitido!');
+    }
+    /* res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n, Angelo');
+    res.end('Hello World,\n Angelo'); */
 });
 
 //subir o servidor
-server.listen(5000); 
+server.listen(PORT, HOST, ()=> {
+    console.log(`Servidor rodando em http://${HOST}:${PORT}`);
+}); 
